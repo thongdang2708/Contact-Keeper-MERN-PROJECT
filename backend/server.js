6bd680a71993c6c640a082a798f0b9acc92e7f62
thongdang2708 <duythong2708@gmail.com>
2022-08-05 10:41:13 +0300

    Add comments

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,24 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 const contactRoute = require("./routes/contactRoutes");
 
+//Connect to database
 connectDB();
 
+//Middleware functionality
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
+//API routes
+
 app.use("/api/users", userRoute);
 app.use("/api/contacts", contactRoute);
 
+//Error handling
+
 app.use(errorHandler);
+
+//Port is listening
 
 app.listen(PORT, () => {
     console.log(`Server is listening at PORT: ${PORT}`.green.underline.bold);
